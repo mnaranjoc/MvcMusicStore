4de686f588e44ae8bb2e4f96e17166f4706efe6f
Manuel Naranjo Contreras <manuelnaranjo@outlook.com>
2020-06-12 14:31:54 -0500

    Models scaffolding and db seeding with recreation on startup

--- MvcMusicStore/MvcMusicStore/Models/IdentityModels.cs
+++ MvcMusicStore/MvcMusicStore/Models/IdentityModels.cs
@@ -29,5 +29,28 @@ namespace MvcMusicStore.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<MvcMusicStore.Models.Album> Albums { get; set; }
+
+        public System.Data.Entity.DbSet<MvcMusicStore.Models.Artist> Artists { get; set; }
+
+        public System.Data.Entity.DbSet<MvcMusicStore.Models.Genre> Genres { get; set; }
+    }
+
+    public class MusicStoreDbInitializer : System.Data.Entity.DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            context.Artists.Add(new Artist { Name = "Al Di Meola" });
+            context.Genres.Add(new Genre { Name = "Jazz" });
+            context.Albums.Add(new Album
+            {
+                Artist = new Artist { Name = "Rush" },
+                Genre = new Genre { Name = "Rock" },
+                Price = 9.99m,
+                Title = "Caravan"
+            });
+            base.Seed(context);
+        }
     }
 }
